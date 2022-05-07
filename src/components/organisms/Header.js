ed7e099039caeaa6e829c33f45a3bd23d7654181
sannaga3 <n.tetsuya02@gmail.com>
2022-05-07 17:23:11 +0900

    ロゴ作成

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -8,9 +8,13 @@ export const Header = () => {
 
   return (
     <header className="py-3 border-b-2 border-gray-500">
-      <div className="flex flex-row justify-left">
-        <div className="px-10 mr-40">
-          <Link to="/">logo</Link>
+      <div className="flex flex-row justify-left space-x-28 items-center">
+        <div className="animate-rotateY border-2 border-black rounded-full shadow-xl bg-white">
+          <div className="border-2 bg-black rounded-full p-2">
+            <Link to="/">
+              <div className="text-white text-lg w-12 h-1 mb-6 ml-4">logo</div>
+            </Link>
+          </div>
         </div>
         <div className="flex flex-row space-x-16">
           <NavLink
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,33 @@ module.exports = {
         "top-img": "url('/public/images/top.jpg')",
         "bee-img": "url('/public/images/bee.jpg')",
       }),
+      animation: {
+        rotateY: "rotateY 2s linear infinite",
+      },
+      keyframes: {
+        rotateY: {
+          "0%": { transform: "rotateY(0)" },
+          "100%": { transform: "rotateY(360deg)" },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-shadow-sm": {
+          textShadow: "2px -2px 2px rgba(0,0,0,0.3)",
+        },
+        ".text-shadow-md": {
+          textShadow: "3px -3px 3px rgba(0,0,0,0.6)",
+        },
+        ".text-shadow-lg": {
+          textShadow: "4px -8px 4px rgba(0,0,0,0.9)",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
   variants: {
     extend: {
       textColor: ["responsive", "hover", "focus", "active"],
